a6ccde128a32e43df4c15010aa26923e9247fe8c
pabloacisera <pabloacisera2021@gmail.com>
2024-08-04 15:20:41 -0300

    update metodo crear turnos

--- src/sist-turnos/sist-turnos.service.ts
+++ src/sist-turnos/sist-turnos.service.ts
@@ -12,7 +12,19 @@ export class SistTurnosService {
 
   async crearTurno(data: { fecha: string, clienteId: number, userId: number }) {
     const fecha = new Date(data.fecha);
-    const turnoExistente = await this.prisma.turno.findFirst({ where: { fecha } });
+
+    // Extraer la fecha en formato YYYY-MM-DD para comparar solo la parte de la fecha
+    const fechaSolo = fecha.toISOString().split('T')[0];
+
+    // Buscar turnos con la misma fecha (sin tener en cuenta la hora exacta)
+    const turnoExistente = await this.prisma.turno.findFirst({
+      where: {
+        fecha: {
+          gte: new Date(fechaSolo + 'T00:00:00.000Z'),
+          lt: new Date(fechaSolo + 'T23:59:59.999Z'),
+        },
+      },
+    });
 
     if (turnoExistente) {
       throw new ConflictException('Turno no disponible');
@@ -30,33 +42,7 @@ export class SistTurnosService {
       console.error('Error al crear el turno:', error);
       throw new Error('Error al crear el turno');
     }
-
-    const turnoAnterior = await this.prisma.turno.findFirst({
-      where: { fecha: new Date(fecha.getTime() - 30 * 60000) }
-    });
-
-    const turnoPosterior = await this.prisma.turno.findFirst({
-      where: { fecha: new Date(fecha.getTime() + 30 * 60000) },
-    });
-
-    const sugerencias = [];
-    if (!turnoAnterior) {
-      sugerencias.push(new Date(fecha.getTime() - 30 * 60000));
-    }
-    if (!turnoPosterior) {
-      sugerencias.push(new Date(fecha.getTime() + 30 * 60000));
-    }
-
-    if (sugerencias.length > 0) {
-      return { mensaje: 'Turno no disponible', sugerencias };
-    }
-
-    return this.prisma.turno.create({
-      data: { fecha, clienteId: data.clienteId, userId: data.userId }
-    });
   }
-
-
 
   async obtenerTurnos() {
     return this.prisma.turno.findMany();
@@ -71,8 +57,8 @@ export class SistTurnosService {
       console.error('Error al obtener los turnos:', error);
       throw new InternalServerErrorException('No se ha podido obtener los turnos');
     }
-  }  
-  
+  }
+
   remove(id: number) {
     return this.prisma.turno.delete({
       where: {
